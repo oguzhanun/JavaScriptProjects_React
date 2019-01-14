b7fc34f6634009319acd47f2a44dd7534fd5a63c
oğuzhan ün <oguzhanun@hotmail.com>
2019-01-14 08:48:21 +0300

    OBS-RTMP-BIG CHANGES

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {streamFetch} from '../../actions';
-
+import flv from 'flv.js';
 
 class StreamShow extends React.Component {
+
+    constructor (props) {
+        super(props);
+        this.videoRef = React.createRef();
+    }
 
     componentDidMount(){
 
         this.props.streamFetch(this.props.match.params.id);
+        this.buildPlayer();
     }
 
-    render(){
-       
+
+    // componentdidmount yalnızca bir kez çalışıyor ve orada çağrılan async streamFetch komutu beklenenden geç bir
+    // zamanda sonuçlanabilir ve bu sırada buildplayer kod bloğu çalıştırılırken stream uygun olmadığında video
+    // tag i de çalışmayacağından videoRef i oluşturamayabiliriz ve yine hata alırız. bunu önlemek için componentDidUpdate
+    // çalıştığında tekrar buildPlayer kodunu çalıştırıyoruz. Eğer this.player hali hazırda varsa kod bloğu çalıştırıl
+    // mayacak. Çünkü varsa buildPlayer içinde öncelikle if ifadesi ile this.player ın varlığı return olmayı gerektiriyor.
+    componentDidUpdate(){
+        this.buildPlayer();
+    }
+
+    buildPlayer(){
+
+        // doesStreamExist metodu ile artık if bloğunun stream i çek eden kısmına gerek kalmadı...
+        if(this.player || !this.props.stream){
+            return;
+        }
+            this.player = flv.createPlayer({
+                type:'flv',
+                url:`http://localhost:8000/live/${this.props.match.params.id}.flv`
+            });
+            this.player.attachMediaElement(this.videoRef.current);
+            this.player.load();
+    }
+
+    componentWillUnmount(){
+        console.log('hi there');
+        this.player.destroy();
+    }
+
+    doesStreamExist(){
         if(!this.props.stream){
             return (
                 <div>
@@ -19,15 +53,23 @@ class StreamShow extends React.Component {
                 </div>
             );
         }
-        
-        const {title, description} = this.props.stream;
+         return(
+            <div>
+                <h2>{this.props.stream.title}</h2>
+                <h4>{this.props.stream.description}</h4>
+            </div>
+        );
+    }
+    render(){
+       
+//        const {title, description} = this.props.stream;
 
         return (
             <div>
+                <video ref={this.videoRef} controls style={{width:'100%'}}/>
                 <br/>
-                <div><h2>{title}</h2></div>
-                <br/>
-                <div><h4>{description}</h4></div>
+                {this.doesStreamExist()}
+                
             </div>
         );
     }
